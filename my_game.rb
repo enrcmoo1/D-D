684896f5681d2ee1218dc91e573b15b1e37f4084
enrcmoo1 <C.Moore1@2010.ljmu.ac.uk>
2012-10-23 15:47:39 +0100

    commit2

--- my_game.rb
+++ my_game.rb
@@ -1,11 +1,31 @@
-player1 = "aaron"
-health = "60"
-puts "#{player1}'s health is #{health}"
+class Player 
+  def initialize(name, health=100)
+    @name = name.capitalize
+    @health = health
+  end
 
-player2 = "molvak"
-player3 = "syntha"
+  def to_s
+    "I'm #{@name} with a health of #{@health}"
+  end
 
-puts "Players:"
-puts "\t#{player1}"
-puts "\t#{player2 b    }"
-puts "\t#{player3}"
+  def blam
+    @health = @health + 25
+  end
+
+  def woot
+    @health = @health + 15
+  end
+end 
+
+player1 = Player.new("aaron")
+player2 = Player.new("syntha", 60)
+player3 = Player.new("molvak", 125)
+
+puts player1.to_s
+puts player2.to_s
+puts player3.to_s
+
+player3.blam
+puts player3.to_s
+player3.woot
+puts player3.to_s
