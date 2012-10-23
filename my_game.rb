9449831289b5523113f6487ea5760f81ca391c3f
enrcmoo1 <C.Moore1@2010.ljmu.ac.uk>
2012-10-23 13:08:22 +0100

    my_game

---
+++ my_game.rb
@@ -0,0 +1,11 @@
+player1 = "aaron"
+health = "60"
+puts "#{player1}'s health is #{health}"
+
+player2 = "molvak"
+player3 = "syntha"
+
+puts "Players:"
+puts "\t#{player1}"
+puts "\t#{player2 b    }"
+puts "\t#{player3}"
